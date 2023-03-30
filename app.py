51fa42e3bf8e9f3da97c2ba57a735523fe64003c
daguang123456 <126739224+daguang123456@users.noreply.github.com>
2023-03-30 14:12:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from img_classification import teachable_machine_classification
+from PIL import Image, ImageOps
 
 st.title("使用谷歌的可教机器进行图像分类")
 st.header("脑肿瘤MRI分类示例")
-st.text("上传脑部MRI图像，将图像分类为肿瘤或无肿瘤")
+st.text("上传彩色脑部MRI的jpg图像，将图像分类为肿瘤或无肿瘤")
 
 uploaded_file = st.file_uploader("选择脑部磁共振成像...", type="jpg")
 if uploaded_file is not None:
