8ea0ae561973ebf85b24310225c96e6176740d76
daguang123456 <126739224+daguang123456@users.noreply.github.com>
2023-03-31 16:57:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title("使用谷歌的可教机器进行图像分类")
 st.header("脑肿瘤MRI分类示例")
 st.text("上传彩色脑部MRI的jpg图像，将图像分类为肿瘤或无肿瘤")
 
-uploaded_file = st.file_uploader("选择脑部磁共振成像...", type="jpg")
+uploaded_file = st.file_uploader("选择脑部磁共振成像...", type=["jpg","png","jpeg"])
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='上传了核磁共振成像。', use_column_width=True)
